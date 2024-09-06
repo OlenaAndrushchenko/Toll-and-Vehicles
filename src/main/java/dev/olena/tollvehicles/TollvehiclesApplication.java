b826a85bda902cebef1fbd1efe98be0109b3738d
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-09-06 22:11:46 +0200

    init commit

---
+++ src/main/java/dev/olena/tollvehicles/TollvehiclesApplication.java
@@ -0,0 +1,13 @@
+package dev.olena.tollvehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollvehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollvehiclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/olena/tollvehicles/TollvehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.olena.tollvehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollvehiclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
